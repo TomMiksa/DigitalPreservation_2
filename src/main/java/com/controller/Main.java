0efc871ddbf04aa6f6485a54efb6e2ecd1a650f1
David Eder <david.eder@me.com>
2018-04-19 17:38:38 +0200

    initial commit

---
+++ src/main/java/com/controller/Main.java
@@ -0,0 +1,39 @@
+package com.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+import java.util.List;
+
+@Controller
+public class Main {
+
+        /*@RequestMapping("/")
+        public String homepage(){
+            return "homepage";
+
+        @RequestMapping(value = "getStationsVienna")
+        @ResponseBody
+        public List<Station> getStationVienna() {
+
+            return dataProcessor.getStationsVienna();
+        }
+
+        @RequestMapping(value = "getStationsBudapest")
+        @ResponseBody
+        public List<Station> getStationBudapest() {
+
+            return dataProcessor.getStationsBudapest();
+        }*/
+
+        public Main(){
+
+        }
+
+        public static void main(String[] args) {
+
+        }
+}
---
+++ src/main/webapp/resources/static/js/app.js
@@ -0,0 +1,11 @@
+var app = angular.module('app', ['ngRoute','ngResource']);
+app.config(function($routeProvider){
+    $routeProvider
+        .when('/chart',{
+            templateUrl: 'resources/static/views/chart.html',
+            controller: 'chartController'
+        })
+        .otherwise(
+            { redirectTo: '/chart'}
+        );
+});
