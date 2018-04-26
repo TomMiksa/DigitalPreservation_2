742b5d361aa653e144c8830464622815d2d20296
David Eder <david.eder@me.com>
2018-04-26 17:35:00 +0200

    report form filled

--- src/main/webapp/resources/static/js/reportController.js
+++ src/main/webapp/resources/static/js/reportController.js
@@ -1,5 +1,62 @@
 app.controller('reportController', function($scope, $http) {
 
+    $( document ).ready(function() {
+        $.ajax({
+            url: "getReport",
+            type: "POST",
+            processData: false,
+            contentType: false,
+            dataType: "json",
+            xhr: function () {
+                var xhr = $.ajaxSettings.xhr();
+                xhr.upload.onprogress = function (evt) {
+                };
+                xhr.upload.onload = function () {
+                    console.log('Upload done.')
+                };
+                return xhr;
+            },
+            success: function (response) {
+                angular.forEach(response, function (value, key) {
+                    if (key == "files") {
+                        angular.forEach(value, function(file, fileKey) {
+                            console.log("file: "+file);
+
+                            $("#filesTable").append("<tr><td><input disabled name='name[]' value='"+file.name+"'>"
+                                +"</td><td><input disabled name='size[]' value='"+file.size+"'>"
+                                +"</td><td><input disabled name='type[]' value='"+file.type+"'>"
+                                +"</td><td><select disabled name='output[]'><option value='true'>Output</option><option value='false'>Input</option></select>"
+                                +"</td><td><input disabled type='number' value='1' min='0' name='quantity[]'></td></tr>")
+
+                        });
+
+                    } else {
+                        $('#' + key + '').text(value);
+                    }
+                    console.log(key + " - " + value);
+
+                });
+            },
+            error: function () {
+                console.log("error", "e");
+            },
+            complete: function () {
+            }
+        });
+
+
+        var d = new Date();
+
+        var month = d.getMonth()+1;
+        var day = d.getDate();
+
+        var output = d.getFullYear() + '/' +
+            (month<10 ? '0' : '') + month + '/' +
+            (day<10 ? '0' : '') + day;
+
+        $('#date').text(output);
+    });
+
 
 
 });
